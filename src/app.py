436dcd8a9e3ea395ed06e5ab5456ca0e599ffeda
chai <chainot@hotmail.co.th>
2024-11-08 19:08:46 +0700

    Unused shutil

--- src/app.py
+++ src/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify, send_from_directory, render_template,
 import json
 import os
 import threading
-import shutil
 
 app = Flask(__name__, static_folder='public', template_folder='public')
 
